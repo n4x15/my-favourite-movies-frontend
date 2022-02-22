6bc0c6cf57b63155b28c1e83919e710e8ddb1490
n4x15 <demych95@mail.ru>
2022-02-22 11:30:06 +0300

    login page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import LoginPage from "./components/LoginPage/LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./components/MainPage/MainPage";
 
 function App() {
+  localStorage.setItem("admin", "admin");
+  localStorage.setItem("login", "password");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/MainPage" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
