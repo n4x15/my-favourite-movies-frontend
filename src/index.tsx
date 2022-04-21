e1ee374ed613ca08db4cde2ff7f35c5c3d8f1711
n4x15 <97973865+n4x15@users.noreply.github.com>
2022-04-21 14:30:31 +0300

    Add apolo client and initialize (#9)
    * Add apolo client and initialize
    
    * Fix url to env

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './i18n';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./i18n";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_URL_TO_GQL,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <Suspense fallback={<h1>Starting</h1>}>
@@ -11,7 +23,7 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </Suspense>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
